bc997257af3ed2541baa1da610294c2dc0b478ed
Abhinav Ennala <abhi.ennala@gmail.com>
2025-11-06 09:43:15 +0530

    06-11-2025

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ mongoose
   })
   .then(() => console.log('db connection successful'));
 
-const port = 8080;
+const port = process.env.PORT || 8080;
 const server = app.listen(port, () => {
   console.log('App is listening on port:', port);
 });
